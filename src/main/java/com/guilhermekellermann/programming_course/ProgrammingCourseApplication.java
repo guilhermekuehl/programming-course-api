23733ce15d79f8e534b0b5de736c1ac4c9896218
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-08-01 02:34:11 -0300

    project created

---
+++ src/main/java/com/guilhermekellermann/programming_course/ProgrammingCourseApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermekellermann.programming_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermekellermann/programming_course/ProgrammingCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermekellermann.programming_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
