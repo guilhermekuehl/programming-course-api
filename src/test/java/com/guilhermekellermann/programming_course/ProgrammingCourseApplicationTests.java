f8b03e236dde0dd0d17a134417341e84447fb164
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-08-16 18:28:17 -0300

    tests cleaned

--- src/test/java/com/guilhermekellermann/programming_course/ProgrammingCourseApplicationTests.java
+++ src/test/java/com/guilhermekellermann/programming_course/ProgrammingCourseApplicationTests.java
@@ -1,13 +1,8 @@
 package com.guilhermekellermann.programming_course;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProgrammingCourseApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
